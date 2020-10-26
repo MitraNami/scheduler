80f63bd513cd5ed5611f2fd226cb2499e68e92f1
Mitra Nami <a.mxnami@gmail.com>
2020-10-26 17:51:14 -0400

    Add formatSpots function that formats the display of props.spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,10 +4,26 @@ import classNames from "classnames"
 import "components/DayListItem.scss";
 
 
+const formatSpots = (spots) => {
+  let msg;
+  switch(spots) {
+    case 0:
+      msg = "no spots remaining";
+      break;
+    case 1:
+      msg = "1 spot remaining";
+      break;
+    default:
+      msg = `${spots} spots remaining`;
+  }
+  return msg;
+};
+
+
 export default function DayListItem(props) {
 
   const { name, spots, selected, setDay } = props;
-  
+
   const listClass = classNames('day-list__item', {
     "day-list__item--selected" : selected,
     "day-list__item--full" : (spots === 0)
@@ -16,7 +32,7 @@ export default function DayListItem(props) {
   return (
     <li className={listClass} onClick={() => setDay(name)}>
       <h2 className="text--regular">{name}</h2> 
-      <h3 className="text--light">{spots}</h3>
+      <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
   );
 }
