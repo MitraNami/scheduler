69916907522737eae0e609cad82dcc691026e401
Mitra Nami <a.mxnami@gmail.com>
2020-10-27 23:10:50 -0400

    Add a story for when Appointment component is empty

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -147,7 +147,12 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="12pm" />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  ))
   .add("Header", () => <Header time="12pm" /> )
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
   .add("Show", () => (
