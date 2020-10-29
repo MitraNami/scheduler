d24d68f5f6888a65e7b9ad5af0e5c3b02b957796
Mitra Nami <a.mxnami@gmail.com>
2020-10-29 15:23:47 -0400

    Add transition functionality

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,7 +3,13 @@ import { useState } from "react";
 
 const useVisualMode = (initial) => {
   const [mode, setMode] = useState(initial);
-  return {mode}
+
+  //takes a new mode and updates the mode state with the new value
+  const transition = (newMode) => {
+    setMode(newMode);
+  };
+
+  return {mode, transition}
 };
 
 export default useVisualMode
