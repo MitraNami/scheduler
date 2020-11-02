61233fd7a9ae591302d23c9552132db948aa8f7f
Mitra Nami <a.mxnami@gmail.com>
2020-11-01 22:36:03 -0500

    Refactor the hook to use reducer

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import axios from 'axios';
 
 
@@ -10,20 +10,55 @@ const findDay = (state, appointmentId) => {
   }
 };
 
+const SET_DAY = "SET_DAY";
+const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+const SET_INTERVIEW = "SET_INTERVIEW";
+
+const reducer = (state, action) => {
+  switch(action.type) {
+
+    case SET_DAY: {
+      const day = action.day;
+      return {...state, day};
+    }
+    case SET_APPLICATION_DATA: {
+      const {days, appointments, interviewers} = action;
+      return {...state, days, appointments, interviewers};
+    }
+    case SET_INTERVIEW: {
+      const {id, interview} = action;
+      const appointment = {
+        ...state.appointments[id], //create a new appointment object starting with the values copied from the existing appointment
+        interview: interview === null ? null : {...interview}
+      };
+      //replace the existing record with the matching id with appointment obj created here
+      const appointments = {
+        ...state.appointments,
+        [id] : appointment
+      };
+      return {...state, appointments};
+
+    }
+    default: {
+      
+    }    
+  }
+};
 
 // deals with data management of Application component
 const useApplicationData = () => {
 
-  //combine the states
-  const [state, setState] = useState({
+
+  const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
     days: [],
     appointments: {},
     interviewers: {}
   });
 
+
   //updates the state with a new day
-  const setDay = day => setState(prev => ({...prev, day}));
+  const setDay = day => dispatch({type: SET_DAY, day});
 
   // we need to update days, appointments, interviewers parts of the state at the same time
   useEffect(() => {
@@ -33,22 +68,22 @@ const useApplicationData = () => {
       axios.get("api/interviewers")
     ]).then(all => {
       const [days, appointments, interviewers] = all.map(item => item.data);
-      setState(prev => ({...prev, days, appointments, interviewers}));
+      dispatch({type: SET_APPLICATION_DATA, days, appointments, interviewers});
     })
   }, []);
   
 
   //pass the function as prop to each Appointment component
   const bookInterview = (id, interview) => {
-    const appointment = {
-      ...state.appointments[id], //create a new appointment object starting with the values copied from the existing appointment
-      interview: {...interview}
-    };
-    //replace the existing record with the matching id with appointment obj created here
-    const appointments = {
-      ...state.appointments,
-      [id] : appointment
-    }
+    // const appointment = {
+    //   ...state.appointments[id], //create a new appointment object starting with the values copied from the existing appointment
+    //   interview: {...interview}
+    // };
+    // //replace the existing record with the matching id with appointment obj created here
+    // const appointments = {
+    //   ...state.appointments,
+    //   [id] : appointment
+    // }
 
     //
     const isCreated = state.appointments[id].interview ? false : true;
@@ -67,14 +102,15 @@ const useApplicationData = () => {
       {interview}
       )
     .then(() => {
-      setState(prev => ({...prev, appointments, days})) //change the state locally if the PUT request is successful
+      //setState(prev => ({...prev, appointments, days})) //change the state locally if the PUT request is successful
+      dispatch({type: SET_INTERVIEW, id, interview})
     });
   };
 
   //pass the function as props to each Appointment component
   const cancelInterview = (id) => {
-    const appointment = {...state.appointments[id], interview : null};
-    const appointments = {...state.appointments, [id] : appointment};
+    // const appointment = {...state.appointments[id], interview : null};
+    // const appointments = {...state.appointments, [id] : appointment};
 
     //Update the spots
     const [day, index] = findDay(state, id);
@@ -84,7 +120,8 @@ const useApplicationData = () => {
 
     return axios.delete(`/api/appointments/${id}`)
       .then(() => {
-        setState(prev => ({...state, appointments, days}))
+        //setState(prev => ({...state, appointments, days}))
+        dispatch({type: SET_INTERVIEW, interview: null});
       });
   };
 
